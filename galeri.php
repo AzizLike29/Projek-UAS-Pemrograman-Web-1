5556c82d202b556c21a84943d3618e4f99e4aace
AzizLike29 <abdulfirdaus590@gmail.com>
2023-02-24 23:16:53 +0700

    Hasil pengerjaan tugas UAS pemrograman web semester 3

---
+++ galeri.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-fluid p-4">
+        <div class="row row-cols-10 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="Gambar/baso aci.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ACI MEATBALL PROMOTION</h5>
+                        <p class="card-text">Bakso aci or what we usually call baso aci is one of the typical culinary of Garut which until now is still loved by many people. This culinary is made from starch or what the Sundanese usually call aci. In appearance, this food is very similar to cilok soup.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 2 mins ago</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="Gambar/cireng suwir.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">CIRENG AYAM PROMOTION</h5>
+                        <p class="card-text">Cireng is a typical Sundanese snack that stands for fried aci (starch). These snacks have a crispy texture on the outside and chewy on the inside. The taste is also savory because it is made from starch dough combined with garlic.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 5 mins ago</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="Gambar/spaghettis bolognaise.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">SPAGHETTIS BOLOGNAISE PROMOTION</h5>
+                        <p class="card-text">Spaghetti Bolognese is a pasta dish that is usually served and has been popular outside Italy such as throughout Europe, North America, Brazil, Australia and New Zealand. Spaghetti Bolognese consists of spaghetti served with Bolognese sauce made from tomatoes, minced beef, garlic, wine and other spices. Sometimes minced beef can be replaced with other minced meat.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 6 mins ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,46 @@
+<div class="container-fluid p-4">
+    <div class="row">
+        <div class="col-lg-4 text-white bg-dark p-5">
+            <h4>Pemrograman web 1</h4>
+            <ul>
+                <li>Nama : Abdul Aziz Firdaus</li>
+                <li>Kelas : TI.21.A.3</li>
+                <li>Nim : 312110262</li>
+                <li>Program Studi : Teknik Informatika</li>
+            </ul>
+        </div>
+        <div class="col-lg">
+            <h1>SALES PROMOTION</h1>
+            <p>Sales promotion is an effort to disseminate or offer a product or service with the aim of attracting potential consumers to buy it. A business business or a distributor will get sales figures with the right promotion that is run properly and correctly.</p>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/baso aci.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">ACI MEATBALL PROMOTION</h5>
+                            <p class="card-text">Bakso aci or what we usually call baso aci is one of the typical culinary of Garut which until now is still loved by many people. This culinary is made from starch or what the Sundanese usually call aci. In appearance, this food is very similar to cilok soup.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/cireng suwir.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">CIRENG AYAM PROMOTION</h5>
+                            <p class="card-text">Cireng is a typical Sundanese snack that stands for fried aci (starch). These snacks have a crispy texture on the outside and chewy on the inside. The taste is also savory because it is made from starch dough combined with garlic.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/spaghettis bolognaise.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">SPAGHETTIS BOLOGNAISE PROMOTION</h5>
+                            <p class="card-text">Spaghetti Bolognese is a pasta dish that is usually served and has been popular outside Italy such as throughout Europe, North America, Brazil, Australia and New Zealand. Spaghetti Bolognese consists of spaghetti served with Bolognese sauce made from tomatoes, minced beef, garlic, wine and other spices. Sometimes minced beef can be replaced with other minced meat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
